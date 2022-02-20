c018f4de032ca575c2aa03564a0d1840ccb8f026
KosovIgor <100098410+KosovIgor@users.noreply.github.com>
2022-02-21 03:06:43 +0400

    Add files via upload

---
+++ HomeworkJavaLesson1/src/main/java/HomeworkJavaLesson1/HomeWorkApp.java
@@ -0,0 +1,52 @@
+package HomeworkJavaLesson1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 1;
+        int b = 4;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная!");
+        }
+        if (a + b < 0) {
+            System.out.println("Сумма отрицательная!");
+        }
+    }
+
+    public static void printColor() {
+        int value = 235;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 10;
+        int b = 276;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}
